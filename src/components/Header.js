4ef79fce9caa6357af1e39afef0bde652232cb1d
Chad <chadjcampbell@gmail.com>
2022-11-28 14:21:31 -0500

    basic flex layout with header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Nintendo Memory</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div className="Main">
+      <h1>Blank for now</h1>
+    </div>
+  );
+}
+
+export default Main;
