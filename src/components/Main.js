78f67e030c87a1d5b0b6a75df3c37c7ab558b06f
Chad <chadjcampbell@gmail.com>
2022-11-28 17:22:02 -0500

    style game cards, add background, tweak footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,9 @@ function Main() {
   return (
     <div className="Main">
       {NES.map((game) => (
-        <div key={game.id} className="gameImg">
-          <img src={game.src} alt={game.name} />
-          <h3>{game.name}</h3>
+        <div key={game.id} className="gameContainer">
+          <img className="gameImg" src={game.src} alt={game.name} />
+          <h3 className="gameH3">{game.name}</h3>
         </div>
       ))}
     </div>
